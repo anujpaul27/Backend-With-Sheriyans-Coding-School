7a84dc1dc537820f1f81122606f13efeeedee22a
anuj paul <paulanuj27@gmail.com>
2026-05-25 06:14:47 +0600

    Added Comment on the routes file

--- Mongoose with JWT auth/src/routes/auth.routes.js
+++ Mongoose with JWT auth/src/routes/auth.routes.js
@@ -2,8 +2,19 @@ const express = require('express')
 const authRouter = express.Router()
 const authController = require('../controllers/auth.controler')
 
+/**
+ *  POST /api/auth/register
+ */
 authRouter.post('/register', authController.register)
+
+/**
+ * GET /api/auth/login
+ */
 authRouter.get('/login',authController.LogIn)
+
+/**
+ * GET /api/auth/refresh-token 
+ */
 authRouter.get('/refresh-token',authController.refreshToken)
 
 
